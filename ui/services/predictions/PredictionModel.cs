67ffa6ef1b0f87288e8da09a076f37240f6c3644
A-ron <codevigilante@gmail.com>
2022-12-01 16:30:38 -0600

    w13: projections displaying;

---
+++ ui/services/predictions/PredictionModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+namespace ui.services.predictions
+{
+	public class PredictionData
+	{
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Team { get; set; }
+        public string Vs { get; set; }
+        public double Workload { get; set; }
+        public string WorkloadTag { get; set; }
+        public double TPG { get; set; }
+        public double FP { get; set; }
+        public string Status { get; set; }
+        public double Salary { get; set; }
+        public double Value { get; set; }
+        //public double BigGameProb { get; set; }
+    }
+
+	public class PredictionModel
+	{
+		public int Season { get; set; }
+        public int Week { get; set; }
+        public DateTime Updated { get; set; }
+        public List<PredictionData> Projections { get; set; } = new List<PredictionData>();
+
+        public List<PredictionData> Sorted()
+        {
+            return (Projections.OrderByDescending(p => p.FP).ToList());
+        }
+    }
+}
+
---
+++ ui/services/predictions/PredictionsService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace ui.services.predictions
+{
+	public class PredictionsService
+	{
+		private static readonly HttpClient Client = new HttpClient();
+        private static readonly string EndpointBase = "https://ottoriot.blob.core.windows.net/predictions";
+
+        public PredictionsService()
+		{
+		}
+
+		public async Task<PredictionModel> Fetch(int season, int week)
+		{
+            string endpoint = $"{EndpointBase}/{season}/pred_w{week}.json";
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, endpoint);
+            request.Headers.Add("Accept", "*/*");
+            request.Headers.Add("User-Agent", "ottoriot web");
+            request.Headers.Add("Cache-Control", "no-cache");
+            request.Headers.Add("Access-Control-Allow-Credentials", "false");
+            request.Headers.Add("Access-Control-Allow-Origin", "true");
+
+            HttpResponseMessage response = await Client.SendAsync(request);
+            PredictionModel model = JsonConvert.DeserializeObject<PredictionModel>(await response.Content.ReadAsStringAsync());
+
+            return (model);
+        }
+	}
+}
+
